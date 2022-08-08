4da47a5415b9ef41ed11b9124d6ffa322b450136
Nicholas Bailey <nickcyg1992@gmail.com>
2022-08-07 20:17:56 -0500

    implement detail page w/ edit and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('places/index/', views.index, name='index'),
   path('places/new/', views.PlaceCreate.as_view(), name='places_create'),
   path('places/list/', views.PlaceList.as_view(), name='places_list'),
+  path('accounts/signup/', views.signup, name='signup'),
+  path('places/<int:pk>/delete/', views.PlaceDelete.as_view(), name='places_delete'),
+  path('places/<int:pk>/update/', views.PlaceUpdate.as_view(), name='places_update'),
+  path('places/<int:place_id>/', views.places_detail, name='places_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Place
 
 
-class PlaceCreate(CreateView):
+class PlaceCreate(LoginRequiredMixin, CreateView):
   model = Place
   fields = ['address']
   success_url = '/places/index/'
@@ -21,7 +24,15 @@ class PlaceCreate(CreateView):
     context['place'] = Place.objects.all()
     return context
 
-class PlaceList(ListView):
+class PlaceUpdate(LoginRequiredMixin, UpdateView):
+  model = Place
+  fields = ['address']
+
+class PlaceDelete(LoginRequiredMixin, DeleteView):
+  model = Place
+  success_url = '/places/index/'
+
+class PlaceList(LoginRequiredMixin, ListView):
   model = Place
   template_name = 'places/list.html'
   context_object_name = 'places'
@@ -33,6 +44,7 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def index(request):
   places = Place.objects.all()
   return render(request, 'places/index.html', { 'places': places })
@@ -51,3 +63,11 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'signup.html', context)
+
+
+class Home(LoginView):
+  template_name = 'home.html'
+
+def places_detail(request, place_id):
+  place = Place.objects.get(id=place_id)
+  return render(request, 'places/detail.html', { 'place': place })
